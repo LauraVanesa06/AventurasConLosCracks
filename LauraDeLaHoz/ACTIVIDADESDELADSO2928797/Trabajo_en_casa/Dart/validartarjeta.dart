32f8f90beeff32192a6db9073d1810de4129b44a
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2024-09-29 18:24:17 -0500

    ... (#10)

---
+++ LauraDeLaHoz/ACTIVIDADESDELADSO2928797/Trabajo_en_casa/Dart/validartarjeta.dart
@@ -0,0 +1,38 @@
+/* invertir orden de los dijios de cada tarjeta.
+2. multiplicar por 2 cada segundo digito empezando por el primero, si da un uumero de 2 digitos se suman.
+3. se suman todos los digitos y es valido si termina en cero.
+*/
+
+import 'dart:io';
+import 'dart:core';
+class Validar{
+int? numTarjeta;
+int variable = 10;
+
+ Validar(numTarjeta){
+  this.numTarjeta = numTarjeta;
+  int numero = numTarjeta;
+  int invertido = 0;
+  while (numTarjeta != 0) {
+  numero % 10;
+	numero ~/= 10;
+  invertido = invertido * variable;
+  int intertidodos = invertido + numero; 
+  }
+  //String canmt = numero.lenght; 
+int canmt = 3; 
+  for (var i = 1; i < canmt; i+=2) {
+     numero[i] *= 2; 
+  }
+
+}
+
+
+}
+
+void main(){
+print("Ingrese poer favoer el númwero de la tarjeta."); 
+int numTarjeta = int.parse(stdin.readLineSync()!);
+Validar num1 = Validar(numTarjeta);
+
+}

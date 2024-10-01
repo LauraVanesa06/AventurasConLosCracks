ebfb53f925744ec69eaff3fe2b0bf0b6d64d69c7
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2024-10-01 07:22:08 -0500

    ...

---
+++ LauraDeLaHoz/ACTIVIDADESDELADSO2928797/Trabajo_en_clase/dart/programacion_orientada_a_objeto/20-09-2024/maps.dart
@@ -0,0 +1,27 @@
+/*import 'petición.dart';
+import 'dart:convert';
+
+class Desconocida {
+  int? userId;
+  int? id;
+   String? title;
+   String? body;
+ 
+  Desconocida(Map decodificar){
+    this.userId = decodificar['userId'];
+    this.id = decodificar['id'];
+    this.title = decodificar['title'];
+    this.body = decodificar['body'];
+  
+  }
+
+    bool statusCode(){
+    return true;
+  }
+}
+
+  void main(){
+  Map<String, dynamic> decodificar = jsonDecode(info);
+
+  }
+  */

525b104a819a574e85b58193e7619e1af0a7929c
KeinerL <keinerlindarte2@gmail.com>
2024-10-07 22:15:13 -0500

    keiner flutter done

--- keiner_lindarte_aro/instructor_henry/mis_programas/flutter/lib/main.dart
+++ keiner_lindarte_aro/instructor_henry/mis_programas/flutter/lib/main.dart
@@ -7,14 +7,24 @@ void main(){
 }
 
 class Myapp extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SENA',
       home: Scaffold(
-        appBar: AppBar(title: Text('DATOS DEL USUARIO:')),
-        body: HomePage(),),
-      );
+        backgroundColor: const Color(0xFFFDFCE4),
+        appBar: AppBar(
+          title: const Center(child: Text('DATOS DEL USUARIO:')),
+          foregroundColor: const Color(0xFFFDFCE4),
+          backgroundColor: const Color(0xFF4BBDFF),
+          ),
+        body: const Column(
+          children: [
+            HomePage()
+          ],),
+      ),
+    );
   }
 
 }

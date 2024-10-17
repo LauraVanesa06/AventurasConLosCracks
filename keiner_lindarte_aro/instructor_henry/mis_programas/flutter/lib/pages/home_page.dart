91e10ea1e7680669e8e4942946fc552b3faee764
KeinerL <keinerlindarte2@gmail.com>
2024-10-17 00:29:58 -0500

    done

--- keiner_lindarte_aro/instructor_henry/mis_programas/flutter/lib/pages/home_page.dart
+++ keiner_lindarte_aro/instructor_henry/mis_programas/flutter/lib/pages/home_page.dart
@@ -4,21 +4,33 @@ import 'package:http/http.dart' as http;
 
 import 'userdata.dart';
 
-
-Future<Map<String, dynamic>> urlData() async{
-  var url = Uri.http('jsonplaceholder.typicode.com', 'users/1');
-  var response = await http.get(url);
-
-  Map<String, dynamic> map = jsonDecode(response.body);
-
-  return map;
-
+class HomePage extends StatefulWidget {
+  @override
+  Info createState(){
+    return Info();
+  }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
+class Info extends State<HomePage> {
+  Future<Map<String, dynamic>>? stateChange;
+  final TextEditingController input = TextEditingController();
+
+  Future<Map<String, dynamic>> dataHttp(String input) async{
+  
+    var url = Uri.http('jsonplaceholder.typicode.com', 'users/$input');
+    await Future.delayed(Duration(seconds: 2));
+    var response = await http.get(url);
+    Map<String, dynamic> map = jsonDecode(response.body);
+
+    return map;
+
+  }
+
+  void changeStateUser() {
+    setState(() {
+      stateChange = dataHttp(input.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,56 +38,64 @@ class HomePage extends StatelessWidget {
       child: Column(
         children: [
           FutureBuilder(
-            future: urlData(),
-            builder: (context, snapshot) {
-          
-              if (snapshot.hasData) {
-                
-                User usuario = User(snapshot.data as Map<String, dynamic>);
-                return Profile(usuario: usuario);
-              
-              }else{
-                return const Center(child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center, 
-                  children: [
-                  CircularProgressIndicator(), 
+            future: stateChange,
+            builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError){
+                  return Text('Error: ${snapshot.error}');
+              } else if (snapshot.hasData){
+                  User user = User(snapshot.data as Map);
+                  return Center(
+                    child: Column(
+                      children: [
+                      Text(''),
+                      Image.network('https://fastly.picsum.photos/id/9/250/250.jpg?hmac=tqDH5wEWHDN76mBIWEPzg1in6egMl49qZeguSaH9_VI'),
+                      Text(''),
+                      Text('ID: ${user.id}'),
+                      Text('Name: ${user.name}'),
+                      Text('Username: ${user.username}'),
+                      Text('Email: ${user.email}'),
+                      Text('Address: ${user.address}'),
+                      Text('Phone: ${user.phone}'),
+                      Text('Website: ${user.website}'),
+                      Text('Company: ${user.company}'),
+                      Text('')
                 ],),);
+              } else {
+                return Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 200,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 30.0),
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            controller: input,
+                            decoration: InputDecoration(
+                              labelText: 'Numero de usuario:',
+                              border: OutlineInputBorder()
+                            ),
+                          ),
+                        ),
+                      ),
+                    ElevatedButton(
+                      onPressed: changeStateUser,
+                      child: Text('Cargar Datos'),
+                    )
+                    ],
+                  ),
+                );
               }
             }
           ),
-          ElevatedButton(onPressed: (){}, child: Text('Buscar')),
-          CircularProgressIndicator()
+          
         ]
       )
     );
   }
-}
-
-class Profile extends StatelessWidget {
-  final User usuario;
-
-  const Profile({super.key, required this.usuario,});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          children: [
-          Text(''),
-          Image.network('https://picsum.photos/250?image=9'),
-          Text(''),
-          Text('ID: ${usuario.id}'),
-          Text('Name: ${usuario.name}'),
-          Text('Username: ${usuario.username}'),
-          Text('Email: ${usuario.email}'),
-          Text('Address: ${usuario.address}'),
-          Text('Phone: ${usuario.phone}'),
-          Text('Website: ${usuario.website}'),
-          Text('Company: ${usuario.company}'),
-          Text(''),
-          ],
-        )
-    );
-  }
-
 }

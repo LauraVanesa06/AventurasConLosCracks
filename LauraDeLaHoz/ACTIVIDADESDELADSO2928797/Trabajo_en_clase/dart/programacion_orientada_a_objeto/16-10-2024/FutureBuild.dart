5f3d8d681c28c876b9b2faa4bd5fdfb2c9b99748
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2024-10-22 21:25:34 -0500

    ...

--- LauraDeLaHoz/ACTIVIDADESDELADSO2928797/Trabajo_en_clase/dart/programacion_orientada_a_objeto/16-10-2024/FutureBuild.dart
+++ LauraDeLaHoz/ACTIVIDADESDELADSO2928797/Trabajo_en_clase/dart/programacion_orientada_a_objeto/16-10-2024/FutureBuild.dart
@@ -8,7 +8,26 @@ void mian(){
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuilContext)
+  
+Widget build(BuildContext context){
+  return Center(
+    child: FutureBuilder<User>(
+      future: fetchData(),
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot){
+        if (snapshot.connectionState == ConnectionState.waiting){
+          // mientra se espera mostrar el indicador o circulito de "Cargando".
+          return CircularProgressIndicator();
+        } else if(snapshot.hasError){
+          //si hay un error, mostrar mensaje avisando la situación.
+          return Text('Error: ${snapshot.error}');
+        } else{
+          User user = snapshot.data;
+          return Text('Name: ${user.name}')
+        }
+      }
+    )
+  )
+}
   
 }
 

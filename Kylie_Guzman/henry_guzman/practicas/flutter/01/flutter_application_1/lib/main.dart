5ca2ad595bace61ec925cf0f4d379e5dc26a9eac
KylieAndrea <kylieguzmanh@gmail.com>
2024-10-25 16:38:25 -0500

    cule codigo malo

---
+++ Kylie_Guzman/henry_guzman/practicas/flutter/01/flutter_application_1/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyProgram());
+}
+
+class MyProgram extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        
+        appBar: AppBar(title: Text('MI PROGRAMA')), 
+        body: Column(
+          children: [
+          
+          ],
+        )
+      ),
+    );
+  }
+}

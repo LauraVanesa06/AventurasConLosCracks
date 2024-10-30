67f650256c08036f51d4d4cc28bc93e2318fd4cb
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2024-10-30 10:03:13 -0500

    ...

--- LauraDeLaHoz/Flutter/flutter_application_1/lib/main.dart
+++ LauraDeLaHoz/Flutter/flutter_application_1/lib/main.dart
@@ -4,29 +4,11 @@ import 'package:flutter_application_1/widget/success.dart';
 import 'models/post.dart';
  
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   Widget build(BuildContext context) {
-    return MaterialApp(
-      //control espacio muestra las opciones que podemos utilizar
-      //materialapp es otra clase en donde se instancia un objeto de tipo
-      //linea lisa herencia y punteada es implement.
-      // polimorfismo se define variable d eun tipo padre y el objeto es de otro tipo hijo.
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(title: Text('SENA')),
-      body:  Column(
-        children:[
-          Text('Id: 1'),
-        ],
-        )
-      ),
-    );
-   }
-}
- /*
+  //clase abstracta que define pero no implementa.
   Future<Post> fetchdata() async {
     final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
     final response = await http.get(url);
@@ -42,35 +24,26 @@ class MyApp extends StatelessWidget {
       //control espacio muestra las opciones que podemos utilizar
       //materialapp es otra clase en donde se instancia un objeto de tipo
       //linea lisa herencia y punteada es implement.
-      // polimorfismo se define variable d eun tipo padre y el objeto es de otro tipo hijo
-      
+      // polimorfismo se define variable d eun tipo padre y el objeto es de otro tipo hijo 
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(title: Text('SENA')),
-      body:  FutureBuilder<Post>( //estado inicial cargando y luego del primer if  o sale bien o mal
-        future: fetchdata(),
-          
-        builder: (BuildContext context, AsyncSnapshot<Post> snapshot){
-        if (snapshot.connectionState == ConnectionState.waiting){
-          // mientra se espera mostrar el indicador o circulito de "Cargando".
-          return CircularProgressIndicator(); //poner el loading
-        } else if(snapshot.hasError){
-          //si hay un error, mostrar mensaje avisando la situación.
-          return Text('Error: ${snapshot.error}'); // retorna ErrorDato
-        } else{
-          Post post = snapshot.data!;
-          return Success(post:post,);
-        }
-      }
-    )
-  )
+        appBar: AppBar(title: Text('titulo')),
+        body:  FutureBuilder<Post>( //estado inicial cargando y luego del primer if  o sale bien o mal
+          future: fetchdata(),
+          builder: (BuildContext context, AsyncSnapshot<Post> snapshot){
+            if (snapshot.connectionState == ConnectionState.waiting){
+            // mientra se espera mostrar el indicador o circulito de "Cargando".
+              return CircularProgressIndicator(); //poner el loading
+            } else if(snapshot.hasError){
+              //si hay un error, mostrar mensaje avisando la situación.
+              return Text('Error: ${snapshot.error}'); // retorna ErrorDato
+            } else{
+              Post post = snapshot.data!;
+              return Success(post:post,);
+            }
+          }
+        )
+      )
     );
+  }
 }
-     
-   }
-
- /*
-
-
-*/  
-  

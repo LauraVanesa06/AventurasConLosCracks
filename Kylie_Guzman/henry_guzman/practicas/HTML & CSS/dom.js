8d442f08325582c643bf2a4cc5c411aaa856b907
KylieAndrea <168228628+KylieAndrea@users.noreply.github.com>
2024-11-27 22:50:37 -0500

    .

--- Kylie_Guzman/henry_guzman/practicas/HTML & CSS/dom.js
+++ Kylie_Guzman/henry_guzman/practicas/HTML & CSS/dom.js
@@ -1,20 +1,26 @@
-fetch('http://jsonplaceholder.typicode.com/photos/1')
+function cambiarInfo() {
+  let id = document.getElementById("fotoId").value;
+  fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
       .then(response => response.json())
-      .then(json => {
-        console.log(json)
-        
-        var a = document.querySelector("body > center > table > tbody > tr:nth-child(1) > td:nth-child(2) > span")
-        a.innerHTML = json.albumId;
+      .then(data => {
 
-        var b = document.querySelector("body > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > span")
-        b.innerHTML = json.id
+        if(data.id){
+          var a = document.querySelector("body > center > table > tbody > tr:nth-child(1) > td:nth-child(2) > span")
+          a.innerHTML = json.albumId;
 
-        var c = document.querySelector("body > center > table > tbody > tr:nth-child(3) > td:nth-child(2) > span")
-        c.innerHTML = json.title
+          var b = document.querySelector("body > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > span")
+          b.innerHTML = json.id
 
-        var d = document.querySelector("body > center > table > tbody > tr:nth-child(4) > td:nth-child(2) > span")
-        d.innerHTML = json.url
+          var c = document.querySelector("body > center > table > tbody > tr:nth-child(3) > td:nth-child(2) > span")
+          c.innerHTML = json.title
 
-        var e = document.querySelector("body > center > table > tbody > tr:nth-child(5) > td:nth-child(2) > span")
-        e.innerHTML =json.thumbnailUrl
+          var d = document.querySelector("body > center > table > tbody > tr:nth-child(4) > td:nth-child(2) > span")
+          d.innerHTML = json.url
+
+          var e = document.querySelector("body > center > table > tbody > tr:nth-child(5) > td:nth-child(2) > span")
+          e.innerHTML =json.thumbnailUrl
+        }else{
+          alert("el ID ingresado NO existe en la base de datos :(")
+        }
       })
+    }
